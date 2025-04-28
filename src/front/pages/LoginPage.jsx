320156c0135ba90f1a1f476621e5a819f6e38336
JosuVentu04 <venturajosue711@gmail.com>
2025-04-28 07:17:17 +0000

    Se termino el formulario para logearse

---
+++ src/front/pages/LoginPage.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer";
+
+export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const IniciarSesion = async (e) => {
+    e.preventDefault();
+  
+    try {
+      if (!email || !password) {
+        alert("¡Todos los campos deben ser llenados!");
+        return;
+      }
+  
+      const cuenta = { email, password };
+  
+      const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cuenta),
+      });
+  
+      const data = await response.json(); // leemos la respuesta
+  
+      if (response.ok) {
+        alert(data.msg || "¡Te has logeado con éxito!");
+        localStorage.setItem("access_token", data.access_token); // <<<<< Guarda el token
+        // navigate("/dashboard"); // si quieres redirigir
+      } else {
+        alert(data.msg || "Error al iniciar sesión.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Hubo un error al procesar la solicitud.");
+    }
+  };
+
+  return (
+    <form className="row justify-content-center p-4" onSubmit={IniciarSesion}>
+      <div className="col-md-6">
+        <h1 className="text-center mb-5">Iniciar Sesión</h1>
+
+        <div className="mb-5">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            id="inputEmail"
+            style={{ borderRadius: "0" }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-5">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Contraseña"
+            id="inputPassword"
+            style={{ borderRadius: "0" }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary w-50">
+            Iniciar Sesión
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
