54b1bd855eca44b961cdeefe89fb802d8833b298
Rubén Ramírez <rruben084@gmail.com>
2025-05-07 02:06:22 +0000

    Agregue case login y case logout

--- src/front/store.js
+++ src/front/store.js
@@ -1,6 +1,8 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     message: null,
+    isAuthenticated: !!sessionStorage.getItem("access_token"),
+    userRole: sessionStorage.getItem("role") || null,
     todos: [
       {
         id: 1,
@@ -11,28 +13,48 @@ export const initialStore=()=>{
         id: 2,
         title: "Do my homework",
         background: null,
-      }
-    ]
-  }
-}
+      },
+    ],
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'set_hello':
+  switch (action.type) {
+    case "set_hello":
       return {
         ...store,
-        message: action.payload
+        message: action.payload,
       };
-      
-    case 'add_task':
 
-      const { id,  color } = action.payload
+    case "add_task":
+      const { id, color } = action.payload;
 
       return {
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        todos: store.todos.map((todo) =>
+          todo.id === id ? { ...todo, background: color } : todo
+        ),
+      };
+
+    case "login":
+      const { token, role } = action.payload;
+      sessionStorage.setItem("access_token", token);
+      sessionStorage.setItem("role", role);
+      return {
+        ...store,
+        isAuthenticated: true,
+        userRole: role,
+      };
+
+    case "logout":
+      sessionStorage.removeItem("access_token");
+      sessionStorage.removeItem("role");
+      return {
+        ...store,
+        isAuthenticated: false,
+        userRole: null,
       };
     default:
-      throw Error('Unknown action.');
-  }    
+      throw Error("Unknown action.");
+  }
 }
