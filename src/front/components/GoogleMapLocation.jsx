d855a028aa5e0bb883fc0098ea20bae7561a7985
Rubén Ramírez <rruben084@gmail.com>
2025-05-12 22:21:29 +0000

    feat(map): actualizar coordenadas del mapa

--- src/front/components/GoogleMapLocation.jsx
+++ src/front/components/GoogleMapLocation.jsx
@@ -3,32 +3,31 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 export const GoogleMapLocation = () => {
     const center = {
-        lat: 40.748817, // Empire State Building latitude
-        lng: -73.985428 // Empire State Building longitude
+        lat: 20.101060, // Latitud de Pachuca, Hidalgo
+        lng: -98.759131 // Longitud de Pachuca, Hidalgo
     };
     return (
         <div>
             <div className="card shadow border-0 mx-auto" style={{ maxWidth: "800px" }}>
-                    <div className="card-body p-0" style={{ overflow: 'hidden', borderRadius: '0.5rem' }}>
-                        <div className="ratio" style={{ aspectRatio: "4/3" }}>
-                            <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-                                <GoogleMap
-                                    mapContainerStyle={{ width: "100%", height: "100%" }}
-                                    center={center}
-                                    zoom={16}   // little closer zoom
-                                    options={{
-                                        fullscreenControl: false,
-                                        mapTypeControl: false,
-                                        streetViewControl: false,
-                                    }}
-                                >
-                                    <Marker position={center} />
-                                </GoogleMap>
-                            </LoadScript>
-                        </div>
+                <div className="card-body p-0" style={{ overflow: 'hidden', borderRadius: '0.5rem' }}>
+                    <div className="ratio" style={{ aspectRatio: "4/3" }}>
+                        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                            <GoogleMap
+                                mapContainerStyle={{ width: "100%", height: "100%" }}
+                                center={center}
+                                zoom={16}
+                                options={{
+                                    fullscreenControl: false,
+                                    mapTypeControl: false,
+                                    streetViewControl: false,
+                                }}
+                            >
+                                <Marker position={center} />
+                            </GoogleMap>
+                        </LoadScript>
                     </div>
                 </div>
+            </div>
         </div>
     )
 }
-
